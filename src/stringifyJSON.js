0f2c8399d47d341dfbc3b1baeaad5a747e2df4f9
Eric Pan <epan@users.noreply.github.com>
2017-03-09 08:11:38 -0800

    Describe an initial high-level plan in comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,23 @@
 
 // but you don't so you're going to write it from scratch:
 
+// High-level plan
+// 1. Be able to detect what data type each node is
+// 2. Based on data type, format/decorate string version
+// 3. Traverse (across and into) entire JSON input
+// 4. Build up string as traversing and then return it
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // determine what type of item it is
+  // will process
+    // number
+    // string
+    // boolean
+    // null (use obj === null)
+    // use Array.isArray or Object.prototype.toString.call
+      // array
+      // object
+  // will not process
+    // function
+    // undefined
 };
