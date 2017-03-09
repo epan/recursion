a08ed7e3be935013e70c6c43a9dc0db4d8dca9d6
Eric Pan <epan@users.noreply.github.com>
2017-03-09 08:26:11 -0800

    Stub functions for detecting types

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,6 +9,22 @@
 // 3. Traverse (across and into) entire JSON input
 // 4. Build up string as traversing and then return it
 
+
+// 1. Be able to detect what data type each node is
+
+var classifyType = function(input) {
+  // detect if input is primitive or object
+  // if input is primitive, return type
+  // if input is object, detect the object and return type
+};
+
+var classifyObject = function(input) {
+  // detect which type of object
+  // return object type
+};
+
+// ORIGINAL FUNCTION
+
 var stringifyJSON = function(obj) {
   // determine what type of item it is
   // will process
