10193daa96ea364b03b165e805865673fd4de114
Eric Pan <epan@users.noreply.github.com>
2017-03-11 14:28:38 -0800

    Add test stubs to a new suite for new smaller tests
    What: Describes stubs for new tests in a new suite that target
    specific data types like flat objects and nested ones separately.
    
    Why: Need more structured testing because I'm going insane.

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,3 +1,14 @@
+// custom test cases
+describe('custom unit stingifyJSON tests', function() {
+  it('should stringify plain primitives', function() {});
+  it('should stringify flat arrays', function() {});
+  it('should stringify flat objects', function() {});
+  it('should stringify objects with nested objects', function() {});
+  it('should stringify arrays with nested arrays', function() {});
+  it('should stringify arrays with nested objects', function() {});
+  it('should stringify objects with nested arrays', function() {});
+});
+
 // test cases are described in fixtures.js
 describe('stringifyJSON', function() {
   it('should match the result of calling JSON.stringify', function() {
